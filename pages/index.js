16b06e3474f7b4be26276f3e39258751b9140fa9
Frank Albenesius <frankalbenesius@gmail.com>
2017-09-14 22:50:59 -0500

    add center circle for spinner

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,34 @@
 import React from 'react'
 
-export default () => <div style={{ color: 'purple' }}>Welcome to Spinner!</div>
+const IndexPage = () => (
+  <div>
+    <div className="circle" />
+    <style jsx>
+      {`
+        .circle {
+          background-color: black;
+          border-radius: 100%;
+          width: 90vmin;
+          height: 90vmin;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+        }
+      `}
+    </style>
+    <style jsx global>
+      {`
+        body {
+          background-color: #ddd;
+          margin: 0;
+        }
+        * > * {
+          box-sizing: border-box;
+        }
+      `}
+    </style>
+  </div>
+)
+
+export default IndexPage
