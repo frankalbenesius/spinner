3c563cc6bf32c6dc0878fb770ba95edef883800d
Frank Albenesius <frankalbenesius@gmail.com>
2017-09-14 23:34:06 -0500

    prepare state display for testing random logic

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,28 @@ import React from 'react'
 
 const IndexPage = () => (
   <div>
-    <div className="circle" />
+    <div className="circle">
+      <div className="testing-state">
+        Current State<br />
+        <br />
+        Quadrants:<br />
+        Q1: 250 | 000 | 00
+        <br />
+        Q2: 250 | 250 | 00
+        <br />
+        Q3: 250 | 500 | 00
+        <br />
+        Q4: 250 | 750 | 00
+        <br />
+        <br />
+        Shrink Factor: 30%<br />
+        <br />
+        Random Number: undefined<br />
+        Winning Quadrant: undefined<br />
+        <br />
+        <button>spin</button>
+      </div>
+    </div>
     <style jsx>
       {`
         .circle {
@@ -14,6 +35,21 @@ const IndexPage = () => (
           top: 50%;
           left: 50%;
           transform: translate(-50%, -50%);
+        }
+        .testing-state {
+          color: white;
+          font-size: 2rem;
+          padding: 5rem 0;
+          text-align: center;
+          font-family: monospace;
+        }
+        .testing-state button {
+          border: none;
+          border-radius: 0.2rem;
+          background-color: white;
+          padding: 1rem 2rem;
+          font-size: 2rem;
+          font-family: monospace;
         }
       `}
     </style>
