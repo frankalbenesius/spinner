37755df2f5f236b5412d297ebf9144659b60df6c
Frank Albenesius <frankalbenesius@gmail.com>
2017-12-23 19:56:11 -0600

    replace preview sound with real sound

--- components/constants.js
+++ components/constants.js
@@ -1,5 +1,5 @@
 export const viewBoxSize = 100
 export const center = viewBoxSize / 2
 export const sectorRadius = 23
-export const spinMs = 5200
+export const spinMs = 4500
 export const sectorColors = ['blue', 'red', 'yellow', 'green']
